e6ed143bf86ed517ce8c435cada59f4440258055
Will Martin <wmartin@costar.com>
2020-04-14 12:03:47 -0400

    garbage collecting custom page code

--- client/src/pages/Custom.js
+++ client/src/pages/Custom.js
@@ -1,32 +1,15 @@
 // react
 import React from 'react';
 import '../styles/Custom.css';
-// PDF
-// import { Document, Page, pdfjs } from 'react-pdf';
-// pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 export default class Custom extends React.Component {
-    // state = { numPages: null, pageNumber: 1 };
-
-    // onDocumentLoadSuccess = ({ numPages }) => {
-    //     this.setState({ numPages });
-    // };
-
-    // goToPrevPage = () => this.setState((state) => ({ pageNumber: state.pageNumber - 1 }));
-    // goToNextPage = () => this.setState((state) => ({ pageNumber: state.pageNumber + 1 }));
-
     render() {
-        // const { pageNumber, numPages } = this.state;
-
         return (
             <div id="pdf-wrapper">
                 <p>Open with Microsoft Edge to enable form filling.</p>
                 <a target="blank" className="btn btn-primary" href="alliance_builders_invoice.pdf">
                     Open PDF
                 </a>
-                {/* <Document file="alliance_builders_invoice.pdf" onLoadSuccess={this.onDocumentLoadSuccess}>
-                    <Page pageNumber={pageNumber} width={850} />
-                </Document> */}
             </div>
         );
     }
