5baea754a4fae3aa5cd100829cd7f05170f5031e
Will Martin <wmartin@costar.com>
2020-04-14 04:45:16 -0400

    fixing deployment issue

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const favicon = require('express-favicon');
 
 const app = express();
 app.use(express.json({ limit: '50mb' }));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 app.use(favicon(__dirname + '/client/build/favicon.ico'));
 
 const port = process.env.PORT || 5000;
