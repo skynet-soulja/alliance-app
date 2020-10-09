bf208b7a567e21473e86cabb28b366a59cca3e5a
skynet-soulja <martinwillnorman@protonmail.com>
2020-10-09 19:11:54 -0400

    add Metro Walk at Moorefield Station - Lofts North job site and state reset on site change

--- client/src/hooks/useInput.js
+++ client/src/hooks/useInput.js
@@ -13,8 +13,11 @@ export default function useInput() {
         } else {
             setInput({ ...input, [event.target.name]: event.target.value });
         }
-    }
 
+        if (event.target.name === "site") {
+            setInput(resetDependants(complexValue, input))
+        }
+    }
     return [input, handleInput];
 }
 
@@ -23,5 +26,21 @@ function initState() {
     for (let component of config.components) {
         stateObj[component.attributes.name] = component.init;
     }
+    return stateObj;
+}
+
+function resetDependants(complexValue, input) {
+    const stateObj = {
+        ...input,
+        site: complexValue
+    };
+
+    for (let component of config.components) {
+        if (component.dependant) {
+            stateObj[component.attributes.name] = component.dependant[complexValue[0]].options[0]
+        }
+    }
+
+
     return stateObj;
 }
